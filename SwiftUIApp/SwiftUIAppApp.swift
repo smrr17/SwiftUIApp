db1d0a6290348252b4ff69111b0d4b4c67bc611b
Smr-tech <shahmeerahmed@zenkoders.com>
2025-05-29 18:46:57 +0500

    Initial Commit

---
+++ SwiftUIApp/SwiftUIAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIAppApp.swift
+//  SwiftUIApp
+//
+//  Created by zenkoders on 29/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SwiftUIAppTests/SwiftUIAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIAppTests.swift
+//  SwiftUIAppTests
+//
+//  Created by zenkoders on 29/05/2025.
+//
+
+import Testing
+@testable import SwiftUIApp
+
+struct SwiftUIAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
